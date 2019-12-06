b6321d78fc5f4bc9591d3b029d86dbe03b174627
wrightben <mail@wrightben.com>
2019-12-06 04:06:07 -0500

    gitall commit

--- hand.sh
+++ hand.sh
@@ -3,8 +3,9 @@
 # Manually type in card values (cards.txt)
 # ./cards.pl < cards.txt > cards.json
 
-
 # Generate card values
 node shuffle.js > cards.json
+
+
 
 node hand.js # Card data read from cards.json
