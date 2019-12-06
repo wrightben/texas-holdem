a045c7857467470137db6f89f8a63089a94c888f
wrightben <mail@wrightben.com>
2019-12-05 23:46:53 -0500

    gitall commit

---
+++ shuffle.js
@@ -0,0 +1,41 @@
+var cards = [
+// 	1, 1, 1, 1, 1, "6h", 1, 1, 1, 1, 1, 1, 1,
+// 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+// 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+// 	1, 1, 1, 1, 1, 1, 1, 1, "9c", "Tc", "Jc", "Qc", "Kc"
+	
+	"Ah", "2h", "3h", "4h", "5h", "6h", "7h", "8h", "9h", "Th", "Jh", "Qh", "Kh",
+	"Ad", "2d", "3d", "4d", "5d", "6d", "7d", "8d", "9d", "Td", "Jd", "Qd", "Kd",
+	"As", "2s", "3s", "4s", "5s", "6s", "7s", "8s", "9s", "Ts", "Js", "Qs", "Ks",
+	"Ac", "2c", "3c", "4c", "5c", "6c", "7c", "8c", "9c", "Tc", "Jc", "Qc", "Kc"
+];
+
+var getDeck = function( players /* (int 0-23) */ ) {
+
+	if ( typeof players == "undefined" ) { return [ "players = 0" ]; }
+
+	var	_ = [],
+		count = 5 + ( players * 2 ),
+		j;
+
+	for ( var i = 0; i < count; i++ ) {
+
+		j = Math.floor( Math.random() * Math.floor( 52 ) );
+
+		while ( cards[j] == 1  ) {
+			// console.log('while: '+j, cards[j]);
+			j = Math.floor( Math.random() * Math.floor( 52 ) );
+		}
+
+		// console.log('push: '+j, cards[j]);
+
+		_.push( cards[j] );
+		cards[j] = 1;
+
+	}
+
+	return _;
+
+}
+
+console.log( JSON.stringify( getDeck( 0 ) ) );
