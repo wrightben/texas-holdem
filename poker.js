c3463631bdb26d6c35ead31c99c19a5e911cd71e
wrightben <mail@wrightben.com>
2019-12-08 01:46:47 -0500

    gitall commit

--- poker.js
+++ poker.js
@@ -1,3 +1,10 @@
+var mask = [	
+	"Ah",	"2h",	"3h",	"4h",	"5h",	"6h",	"7h",	"8h",	"9h",	"Th",	"Jh",	"Qh",	"Kh",	
+	"Ad",	"2d",	"3d",	"4d",	"5d",	"6d",	"7d",	"8d",	"9d",	"Td",	"Jd",	"Qd",	"Kd",	
+	"As",	"2s",	"3s",	"4s",	"5s",	"6s",	"7s",	"8s",	"9s",	"Ts",	"Js",	"Qs",	"Ks",	
+	"Ac",	"2c",	"3c",	"4c",	"5c",	"6c",	"7c",	"8c",	"9c",	"Tc",	"Jc",	"Qc",	"Kc"
+];
+
 var cards = [
 	0,	1,	2,	3,	4,	5,	6,	7,	8,	9,	10,	11,	12,
 	13,	14,	15,	16,	17,	18,	19,	20,	21,	22,	23,	24,	25,		
@@ -34,13 +41,13 @@ var getDeck = function( players /* (int 0-23) */ ) {
 // Evaluate hands
 var	evaluateHand = function( _array ) {
 
-	var	_l = _array.length,
-		_ = [
-			[	// Values
+	// COLLECTION
+	var	_ = [
+			[	// Values (0s, 1s, 2s, 3s ...)
 				[], [], [], [], [], [], [], [], [], [], [], [], []
 				
 			],
-			[	// Suits
+			[	// Suits (0-12), (13-25), (26-38), (39-51)
 				[], [], [], []
 			] 
 			/*
@@ -51,56 +58,60 @@ var	evaluateHand = function( _array ) {
 
 
 	// Group cards by value, suit
-	var	i = 0,
-		c, s, r; // card, suit, rank
+	var	i = 0, 
+		_l = _array.length,
+		c, s, h = {}; // card, suit, r (result object)
 
 	for (i = 0; i < _l; i++ ) {
 		c = _array[i];
-		s = Math.floor(c / 13);
-		
+		s = Math.floor(c / 13);	
 		_[ 0 ][ c - (s*13) ].push(c);
 		_[ 1 ][s].push(c);
 	}
 	
+	// COLLECTION
+	var _r = [ 
+		// High Cards, Pairs, Trips, Quads
+		[],[],[],[] 
+	];
 	
-	// High Cards, Pairs, Trips, Quads
-	var _r = [ [],[],[],[] ];
+	
+	// Group cards by HC, Pairs, Trips, Quads
 	for (i = 0; i < 13; i++) {
 		if (_[0][i].length > 0) {
 			_r[_[0][i].length - 1].push(i);
 		}
 	}
-	
-	// If straight counter ends at 4, is there a 0(ace)?
-	
-	
-	// Flush ? sort, isStraight, setRank
-	for (i = 0; i < 4; i++) {
-		if (_[1][i].length > 4) {
-			_[1][i].sort();
-			console.log( 'Flush: '+ _[1][i]);
-		}
-	}
-	
+
+	// RANK, VALUE: uses _r
 	if ( _r[3].length > 0 ) { // Four
-		console.log("Four of a Kind");
+		h.rank = 8;
+		h.value = _r[3];
 	} else if ( _r[2].length > 1 || (( _r[2].length > 0) && (_r[1].length > 0)) ) { // Full House
-		console.log("Full House");	
+		h.rank = 7;
+		h.value = [ _r[2][0], _r[1][0] ];
 	} else if ( _r[2].length > 0 ) { // Trips
-		console.log("Trips");
+		h.rank = 4;
+		h.value = _r[2];
 	} else if ( _r[1].length > 1 ) { // Pair x 2
-		console.log("2 Pair");		
+		h.rank = 3;
+		h.value = _r[1];
 	} else if ( _r[1].length > 0 ) { // Pair x 1
-		console.log("1 Pair");
+		h.rank = 2;
+		h.value = _r[1];
 	} else { // High Card
-		console.log("High Card");
-		(_r[0][0] == 0) ? console.log(_[0][0]) : console.log(_r[0][0]); // Only 1 x HC in collection; Either 0 || !0 (Ace, Not Ace)
+		h.rank = 1;
+		(_r[0][0] == 0) ? h.value = _[0][0] : h.value = _r[0]; // Only 1 x HC in collection; Either 0 || !0 (Ace, Not Ace)
 	}
 
-	console.log(_r);
-
-	_.push( _array );
-	return _;
+	
+	h.cards = _array;
+	h.hand = {
+		"_" : _,
+		"_r" : _r
+	};
+	
+	return h;
 
 }
 
@@ -112,4 +123,5 @@ var	players = 0,
 	deck = getDeck( players );
 	
 //console.log( JSON.stringify(deck) );	
-console.log( evaluateHand([4, 17, 12, 43, 30]) );
+console.log( JSON.stringify(evaluateHand([18, 44, 31, 5, 4])) );
+//evaluateHand([26, 14, 20, 5, 4]);
