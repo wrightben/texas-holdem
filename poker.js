72bc48ae0a76a84651865c4ab19f11b2d1d689e7
wrightben <mail@wrightben.com>
2019-12-09 08:52:38 -0500

    gitall commit

--- poker.js
+++ poker.js
@@ -6,6 +6,7 @@ var faceValues = [
 ];
 
 var numbers = [
+//	A	2	3	4	5	6	7	8	9	10	J	Q	K	A
 	0,	1,	2,	3,	4,	5,	6,	7,	8,	9,	10,	11,	12,
 	13,	14,	15,	16,	17,	18,	19,	20,	21,	22,	23,	24,	25,		
 	26,	27,	28,	29,	30,	31,	32,	33,	34,	35,	36,	37,	38,		
@@ -281,6 +282,6 @@ var	players = 7,
 
 
 	console.log( JSON.stringify(compareHands(hands)) );
-// 	console.log( JSON.stringify(evaluateHand( [24,25,23,4,13,39,22] )) );
+// 	console.log( JSON.stringify(evaluateHand( [44, 39, 43, 24, 51, 2, 49] )) );
 // 	console.log( JSON.stringify(evaluateHand( [1,14,27,40] )) );
 //	console.log( JSON.stringify(evaluateHand( cards )) );
