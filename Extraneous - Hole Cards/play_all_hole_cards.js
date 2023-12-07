de81e33f8666b2a6fde6e0429d91b3903f86eae1
Benjamin Wright <mail@wrightben.com>
2023-12-07 02:59:35 -0500

    gitall commit

---
+++ Extraneous - Hole Cards/play_all_hole_cards.js
@@ -0,0 +1,8 @@
+// Idea: Play every hand 500,000 times against n opponents
+
+// console.log("Current directory:", __dirname);
+
+const hole_cards = require('./hole_cards.json');
+const texas_holdem = require('./../texas-holdem');
+
+console.log(hole_cards[1]);
