4e9b06ddf160c51dd75ba84d0b767369cbb08571
Benjamin Wright <mail@wrightben.com>
2023-12-07 03:01:32 -0500

    gitall commit

--- Extraneous - Hole Cards/play_all_hole_cards.js
+++ Extraneous - Hole Cards/play_all_hole_cards.js
@@ -5,4 +5,27 @@
 const hole_cards = require('./hole_cards.json');
 const texas_holdem = require('./../texas-holdem');
 
-console.log(hole_cards[1]);
+// console.log(hole_cards[1]);
+
+hole_cards.forEach(function(e) {
+
+	for (var i = 1; i < 500; i++) {
+
+// 		var	players = 6,
+// 			shared = 5,	// community cards
+// 	
+// 			cards = getCards( players, shared );	// shared optional; 5 default
+// 	
+// 			hands = evaluateHands( 
+// 				getCardsAsPlayers(players, cards)
+// 			);
+// 	
+// 			bestHands = getBestHands(hands); // see comment @return in getBestHands
+// 	
+// 		console.log( JSON.stringify(bestHands) );
+
+		console.log(i);
+	
+	};
+
+});
