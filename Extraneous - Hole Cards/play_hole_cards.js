59ecbd24f9f06bc1cda9ecec3e0ac973eaeae6ab
Benjamin Wright <mail@wrightben.com>
2023-12-08 07:34:38 -0500

    gitall commit

--- Extraneous - Hole Cards/play_hole_cards.js
+++ Extraneous - Hole Cards/play_hole_cards.js
@@ -25,22 +25,25 @@ var results = [];
 	
 for (var i = 1; i <= 50000; i++) {
 
-	var	players = 9,
+	var	players = 6,
 		shared = 5,
 
 	cardsAsPlayers = texas_holdem.getCardsAsPlayers(players, 
 		texas_holdem.getCards([players, [
-				_hc				
-// 				texas_holdem.getCardsFromString("js jh"),
-// 				texas_holdem.getCardsFromString("as ah")
+				_hc // GAME: from hole_cards[index]				
+// 				texas_holdem.getCardsFromString("js jh"), texas_holdem.getCardsFromString("as ah") // GAME: From hole cards as string
 			]
 		], shared)
 	);
 
+
+	// GAME: players + shared from STRING	
+// 	players = 6;
+// 	cardsAsPlayers = texas_holdem.getCardsAsPlayers( players, texas_holdem.getCardsFromString("2d 7c tc 3c jd 7s th as kc kd 6c ad 8s 5d 2s td 2c") );
+		
+
 	hands = texas_holdem.evaluateHands( cardsAsPlayers );
-
 	bestHands = texas_holdem.getBestHands(hands);
-
 	results.push(bestHands);
 
 };
