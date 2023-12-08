610666cf13babdbe815c82c89d40456bc839b34d
Benjamin Wright <mail@wrightben.com>
2023-12-08 03:02:45 -0500

    gitall commit

--- Extraneous - Hole Cards/README.md
+++ Extraneous - Hole Cards/README.md
@@ -27,23 +27,3 @@ Player 0 was dealt hole_cards[0] 1 time in a 6-person game.
 ### Results of Scenarios
 Player 0 is dealt the specified hole cards N times in an N-person game. Try to define *bluff* in this context.
 
-313 [ 25, 13 ] kd ad<br />
-30652 0.32265263157894736
-
-665 [ 36, 35 ] js ts<br />
-25055 0.26373684210526316
-
-78 [ 13, 0 ] ad ah<br />
-23255 0.24478947368421053
-
-300 [ 25, 0 ] kd ah<br />
-21811 0.22958947368421054
-
-0 [ 1, 0 ] 2h ah<br />
-12821 0.1349578947368421
-
-631 [ 36, 1 ] js 2h<br />
-12458 0.13113684210526316
-
-211 [ 21, 1 ] 9d 2h<br />
-10474 0.11025263157894737
--- Extraneous - Hole Cards/do.sh
+++ Extraneous - Hole Cards/do.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-v=640
+summary () {
+	node lookup_hole_cards.js ${1}
+	node play_hole_cards.js ${1} > data_${1}_1.json
+	node read_data.js data_${1}_1.json
+}
 
-node lookup_hole_cards.js ${v}
-node play_hole_cards.js ${v} > data_${v}_1.json
-node read_data.js data_${v}_1.json
 
-
-v=78
-
-node lookup_hole_cards.js ${v}
-node play_hole_cards.js ${v} > data_${v}_1.json
-node read_data.js data_${v}_1.json
-
-v=715
-
-node lookup_hole_cards.js ${v}
-node play_hole_cards.js ${v} > data_${v}_1.json
-node read_data.js data_${v}_1.json
-
-v=66
-
-node lookup_hole_cards.js ${v}
-node play_hole_cards.js ${v} > data_${v}_1.json
-node read_data.js data_${v}_1.json
+summary 640
+summary 78
+summary 715
+summary 66
+summary 313
+summary 665
+summary 300
+summary 0
+summary 631
+summary 211
