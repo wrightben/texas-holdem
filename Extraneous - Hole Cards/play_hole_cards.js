6c63163dae8c00aca150e217c925116064c7f37e
Benjamin Wright <mail@wrightben.com>
2023-12-16 02:29:30 -0500

    gitall commit

--- Extraneous - Hole Cards/play_hole_cards.js
+++ Extraneous - Hole Cards/play_hole_cards.js
@@ -23,24 +23,29 @@ if (typeof process.argv[2] != "undefined") {
 
 var results = [];
 	
-for (var i = 1; i <= 50000; i++) {
+for (var i = 1; i <= 1; i++) {
 
 	var	players = 9,
 		shared = 5,
 
-	cardsAsPlayers = texas_holdem.getCardsAsPlayers(players, 
-		texas_holdem.getCards([players, [
-				_hc // GAME: from hole_cards[index]				
-// 				texas_holdem.getCardsFromString("js jh"), texas_holdem.getCardsFromString("as ah") // GAME: From hole cards as string
-			]
-		], shared)
-	);
+// 	cardsAsPlayers = texas_holdem.getCardsAsPlayers(players, 
+// 		texas_holdem.getCards([players, [
+// 				_hc // GAME: from hole_cards[index]				
+// // 				texas_holdem.getCardsFromString("js jh"), texas_holdem.getCardsFromString("as ah") // GAME: From hole cards as string
+// 			]
+// 		], shared)
+// 	);
 
 
 	// GAME: from hole cards and shared cards as string	
-// 	players = 6;
-// 	cardsAsPlayers = texas_holdem.getCardsAsPlayers( players, texas_holdem.getCardsFromString("2d 7c tc 3c jd 7s th as kc kd 6c ad 8s 5d 2s td 2c") );
-		
+	players = 6;
+// 	cardsAsPlayers = texas_holdem.getCardsAsPlayers( players, texas_holdem.getCardsFromString(
+// 		"ad 3h qc 3s th 6d as qd jc 9h 5c kh 9s 4s ah kd 2s"
+// 	) );
+
+	cardsAsPlayers = texas_holdem.getCardsAsPlayers( players, texas_holdem.getCardsFromString(
+		process.argv[2]
+	) );		
 
 	hands = texas_holdem.evaluateHands( cardsAsPlayers );
 	bestHands = texas_holdem.getBestHands(hands);
